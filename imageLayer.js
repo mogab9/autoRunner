d879337e072df99e90d2b29c69ab1102005b71bc
mogab9 <mogab9@gmail.com>
2013-02-16 11:18:00 +0100

    Imagelayer: Utilisation du ratio pour la vitesse plutot qu'une variable speed en plus

--- imageLayer.js
+++ imageLayer.js
@@ -1,13 +1,11 @@
 /*global me*/
 
 var ScrollingBackgroundLayer = me.ImageLayer.extend({
-  init: function (image, speed) {
+  init: function (image, ratio) {
     var name = image;
     var width = 640;
     var height = 480;
     var z = 1;
-    var ratio = 1;
-    this.speed = speed;
     this.parent(name, width, height, image, z, ratio);
   },
 
@@ -17,7 +15,7 @@ var ScrollingBackgroundLayer = me.ImageLayer.extend({
       this.offset.x = 0;
     }
     // increment horizontal background position
-    this.offset.x += this.speed;
+    this.offset.x += this.ratio;
     return true;
   }
 });
